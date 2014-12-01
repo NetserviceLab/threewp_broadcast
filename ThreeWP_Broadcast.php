54511b31832d576ad101988de2e658a15409ccc2
Edward Plainview <edward@plainview.se>
2014-12-01 19:50:27 +0100

    Finally added SDK

--- ThreeWP_Broadcast.php
+++ ThreeWP_Broadcast.php
@@ -24,6 +24,6 @@ function ThreeWP_Broadcast()
 
 // For compatability with old SDK
 if ( ! class_exists( '\\plainview\\sdk\\collections\collection' ) )
-	require_once( 'include/old_sdk/collection.php' );
+	require_once( 'src/old_sdk/collection.php' );
 
 $threewp_broadcast = new threewp_broadcast\ThreeWP_Broadcast();
