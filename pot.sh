8c0c1356cfd5d580927056c548d414c9696385e7
Edward Plainview <edward@plainview.se>
2014-12-01 18:53:22 +0100

    New paths to sdk

--- pot.sh
+++ pot.sh
@@ -4,11 +4,11 @@
 
 xgettext -s -c --no-wrap -d threewp_broadcast\ThreeWP_Broadcast -p lang -o "lang/ThreeWP_Broadcast.pot" \
     --omit-header -k_ -kerror_ -kdescription_ -kmessage_ -kheading_ -klabel_ -kname_ -koption_ -kp_ -ktext_ -kvalue_ \
-    vendor/plainview/sdk/*php vendor/plainview/sdk/form2/inputs/*php vendor/plainview/sdk/form2/inputs/traits/*php vendor/plainview/sdk/wordpress/*php \
-    include/*php \
-    include/traits/*php
+    src/sdk/sdk/*php src/sdk/sdk/form2/inputs/*php src/sdk/sdk/form2/inputs/traits/*php src/sdk/sdk/wordpress/*php \
+    src/*php \
+    src/traits/*php
 
 xgettext -s -c --no-wrap -d threewp_broadcast\blog_groups\ThreeWP_Broadcast_Blog_Groups -p lang -o "lang/ThreeWP_Broadcast_Blog_Groups.pot" \
-    --omit-header -k_ -kerror_ -kdescription_ -kmessage_ -kheading_ -klabel_ -kname_ -koption_ -kp_ -ktext_ -kvalue_ ThreeWP_Broadcast_Blog_Groups.php vendor/plainview/sdk/*php vendor/plainview/sdk/form2/inputs/*php vendor/plainview/sdk/form2/inputs/traits/*php \
-    include/blog_groups/*php \
-    vendor/plainview/sdk/wordpress/*php
+    --omit-header -k_ -kerror_ -kdescription_ -kmessage_ -kheading_ -klabel_ -kname_ -koption_ -kp_ -ktext_ -kvalue_ ThreeWP_Broadcast_Blog_Groups.php src/sdk/sdk/*php src/sdk/sdk/form2/inputs/*php src/sdk/sdk/form2/inputs/traits/*php \
+    src/blog_groups/*php \
+    src/sdk/sdk/wordpress/*php
