33a125cd4b9b62a6d38c479eebe3f67314311a3f
Edward Plainview <edward@plainview.se>
2015-01-16 15:41:51 +0100

    Version bump.

--- ThreeWP_Broadcast.php
+++ ThreeWP_Broadcast.php
@@ -6,10 +6,10 @@ Author URI:		http://plainviewplugins.com
 Description:	Broadcast / multipost a post, with attachments, custom fields, tags and other taxonomies to other blogs in the network.
 Plugin Name:	ThreeWP Broadcast
 Plugin URI:		http://plainviewplugins.com/threewp-broadcast/
-Version:		15
+Version:		16
 */
 
-DEFINE( 'THREEWP_BROADCAST_VERSION', 15 );
+DEFINE( 'THREEWP_BROADCAST_VERSION', 16 );
 
 require_once( __DIR__ . '/vendor/autoload.php' );
 
